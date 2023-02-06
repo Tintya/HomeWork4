843acc65738dc83cc97fa8d9c70d7f850c151ab0
Tintya <tintya@mail.ru>
2023-02-06 14:06:28 +0500

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или не корректное число.");
+        }
+    }
+
+    return result;
+}
+
+int GetSum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+int number = GetNumber("Введите число больше 0");
+int sum = GetSum(number);
+System.Console.WriteLine($"Сумма всех цифр числа {number} = {sum}");
